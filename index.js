6a7168e04c9945ca2fe51eca9ae901e63fbe02fc
jeferybeckam <you@example.com>
2024-12-18 19:29:29 +0530

    link

---
+++ index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBkKkGpsW8IHYhj58IB2fnSwbcKHpUO4Fs",
+    authDomain: "cafex-db3f0.firebaseapp.com",
+    projectId: "cafex-db3f0",
+    storageBucket: "cafex-db3f0.firebasestorage.app",
+    messagingSenderId: "208819280946",
+    appId: "1:208819280946:web:cb246daad100dad86cb02b",
+    measurementId: "G-ST6ETC7HL2"
+  };
